fb224e51efa7fe524876c65751b9d8f1afb7737a
沈经纬 <327538014@qq.com>
2018-08-05 21:07:09 +0800

    build: delete some useless contents

--- build/build.js
+++ build/build.js
@@ -31,11 +31,5 @@ rm(path.resolve(__dirname, '../dist'), err => {
     }
 
     console.log(chalk.cyan('  Build complete.\n'));
-    console.log(
-      chalk.yellow(
-        '  Tip: built files are meant to be served over an HTTP server.\n' +
-          "  Opening index.html over file:// won't work.\n"
-      )
-    );
   });
 });
